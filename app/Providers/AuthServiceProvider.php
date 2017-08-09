e37a53f2d8cf79735abab3ee1499298dc16cd875
qiutuleng <qiutuleng@gmail.com>
2017-08-09 15:41:12 +0800

    Setting token expire time

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::tokensExpireIn(\Carbon\Carbon::now()->addDays(15));
     }
 }
