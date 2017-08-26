65b97ffb1d8b5e9a89424d07f298321729581b7f
qiutuleng <qiutuleng@gmail.com>
2017-08-26 17:59:26 +0800

    Rewrite user_Id to user_id

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -20,7 +20,7 @@ class UserTransformer extends BaseTransformer
     public function transform(User $model)
     {
         return [
-            'user_Id' => (int)$model->getKey(),
+            'user_id' => (int)$model->getKey(),
             'name' => (string)$model->name,
             'email' => (string)$model->email,
             'created_at' => (string)$model->created_at,
