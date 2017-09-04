bafdb3cdd6816b678049490cbfb18333bbe18d29
qiutuleng <qiutuleng@gmail.com>
2017-09-04 23:14:17 +0800

    Import SoftDelete trait

--- app/Models/ClassModel.php
+++ app/Models/ClassModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Base\BaseUserModel as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
