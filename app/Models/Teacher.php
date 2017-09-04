1d29d1a557ac45e9c5eb5ef2287769bdac2ee958
qiutuleng <qiutuleng@gmail.com>
2017-09-04 23:01:24 +0800

    Use BelongsToUser trait

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Base\BaseUserModel as Model;
+use App\Models\Traits\BelongsToUser;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
@@ -10,6 +11,7 @@ use Prettus\Repository\Traits\TransformableTrait;
 class Teacher extends Model implements Transformable
 {
     use TransformableTrait, SoftDeletes;
+    use BelongsToUser;
 
     protected $primaryKey = 'teacher_id';
 
