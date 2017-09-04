800a7dc476af8ae1b8ab6f86343c9a1a57e83136
qiutuleng <qiutuleng@gmail.com>
2017-09-04 23:07:23 +0800

    Improve BelongsToUser trait

--- app/Models/Traits/BelongsToUser.php
+++ app/Models/Traits/BelongsToUser.php
@@ -6,10 +6,21 @@ use App\Models\User;
 
 trait BelongsToUser
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         static $user_primary_key;
         $user_primary_key = $user_primary_key ?? with(new User())->getKeyName();
         return $this->belongsTo(User::class, $user_primary_key, $user_primary_key);
+    }
+
+    /**
+     * @return User|null
+     */
+    public function getUser()
+    {
+        return $this->user;
     }
 }
